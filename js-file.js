8c4baca622296290394c7b0c77e0409f6bc10904
Sanchit <sk010us@gmail.com>
2025-04-17 14:46:17 -0700

    useless

--- js-file.js
+++ js-file.js
@@ -1,8 +1,1 @@
 // your JavaScript file
-const container = document.querySelector("#container");
-
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content2!";
-
-container.appendChild(content);
